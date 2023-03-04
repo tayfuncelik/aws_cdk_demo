7deb604f9f35e83bb3b9fe037f9396c7b7b14c7c
tayfuncelik <tayfuncelikeee@gmail.com>
2023-03-05 02:52:52 +0300

    first commit1

--- README.md
+++ README.md
@@ -12,3 +12,24 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `cdk deploy`      deploy this stack to your default AWS account/region
 * `cdk diff`        compare deployed stack with current state
 * `cdk synth`       emits the synthesized CloudFormation template
+
+
+1- Go to Developer Settings in Github and copy Personal Access Tokens
+ *create new token by selectig
+ -repo
+ -admin:repo-hook
+ generate token.
+
+
+2-We should add this token to be able to create an access between GitHub and AWS
+   Open AWS "Secret Manager"    
+   create a new secret
+   -other type of secret
+   -paste your token into plaintext tab
+
+3- now you can create initial version of pipeline by running " cdk deploy" using terminal
+
+4- check "cloud formation"  if our "stack" is "CREATE_IN_PROGRESS" it will take some time to complete
+  *you can go to "Events"and change preferences and you can show/hide necessary colums to be able to see Type,Status...etc
+
+5- open "CodePipeline" by using search box and you can see all changes regarding to deployment by CDK
---
+++ lib/ci-cd-aws-pipeline-stack.ts
@@ -0,0 +1,40 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep, Step } from 'aws-cdk-lib/pipelines';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './stage';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CiCdAwsPipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('beabetterdevv/ci-cd-aws-pipeline-demo', 'main'), //Remember to change 
+        commands: ['npm ci', 
+                   'npm run build', 
+                   'npx cdk synth']
+      })
+    });
+
+    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "175560813486", region: "us-east-1" }
+    }));
+
+
+    testingStage.addPre(new ShellStep("Run Unit Tests", { commands: ['npm install', 'npm test'] }));
+    testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+
+    const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
+      env: { account: "175560813486", region: "us-east-1" }
+    }));
+
+    
+    // example resource
+    // const queue = new sqs.Queue(this, 'AwsQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
